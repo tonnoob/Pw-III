5b68305d1747585dfdcef473b44d47158bb9d4af
tonnoob <ottonsouzanto@gmail.com>
2023-05-18 22:21:20 -0300

    Finalizando as APIs do reclamacao e laboratorio

---
+++ Aula12/app/Http/Controllers/ReclamacaoController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Reclamacao;
+use App\Laboratorio;
+
+class ReclamacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reclamacoes = Reclamacao::all();
+
+        $laboratorios = Laboratorio::all();
+
+        return view('Reclamacao', compact('reclamacoes','laboratorios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $reclamacao = new Reclamacao();
+        $reclamacao-> pc= $request ->txPc;
+        $reclamacao -> titulo = $request -> txTitulo;
+        $reclamacao -> descricao = $request -> txDescricao;
+        $reclamacao -> dtCriacao = $request -> txDtCriacao;
+        $reclamacao -> idLaboratorio = $request -> idLaboratorio;
+
+        $reclamacao -> save();
+
+        return redirect('/reclamacao');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($idRec)
+    {
+        $reclamacao = new Reclamacao();
+        $reclamacao->where('idRec','=',$idRec)->delete();
+        return redirect('/reclamacao');
+    }
+
+    public function reclamacaoLista()
+    {
+        $reclamacoes = Reclamacao::all();
+   
+        return view('reclamacao',compact('reclamacoes'));
+    }
+
+    public function allClaims(){
+        $reclamacoes = Reclamacao::all();      
+        return $reclamacoes;
+    }
+
+
+}
---
+++ Aula12/resources/views/contato.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul>
+        <li><a href="/">Principal</a></li>
+        <li><a href="/quemsomos">Quem Somos</a></li>
+        <li><a href="/reclamacao">Reclamacao</a></li>
+        <li><a href="/laboratorio">Laboratorios</a></li>
+    </ul>
+    
+<form action="/contato" method="post">        
+        {{csrf_field()}}        
+        <input type="text" name="txNome" placeholder="Nome"/>
+        <input type="text" name="txEmail" placeholder="E-mail" />
+        <input type="text" name="txAssunto" placeholder="Assunto" />
+        <input type="text" name="txMensagem" placeholder="Mensagem" />
+        <input type="submit" value="Salvar" />
+    </form>
+
+    @foreach($contatos as $c)        
+        <p>
+        {{$c->idContato}}
+        {{$c->nome}}
+        {{$c->email}}
+        {{$c->assunto}}
+        {{$c->mensagem}}
+        <a href="/contato/excluir/{{$c->idContato}}"> Excluir </a>
+        </p>
+    @endforeach
+
+    <select name="selContato">
+    @foreach($contatos as $c)
+    <option value="{{$c->idContato}}"> {{$c->nome}}  </option>    
+    @endforeach
+    </select>
+
+</body>
+</html>
